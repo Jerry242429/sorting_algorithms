e4f70f5c143c1a5bd25a9ea11db22dd7a2a434b1
Nardos <contactnardos@gmail.com>
2023-12-19 22:09:27 +0000

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,20 +8,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int isSorted = 0, i = 0, j = 0, k = 0, temp;
+	size_t isSorted = 1, i = 0, j = 0, temp;
 
 	for (i = 0; i < size - 1 && isSorted; i++)
 	{
-		for (j = i; i < size - 1; j++)
+		isSorted = 0;
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i+1])
+			if (array[j] > array[j + 1])
 			{
 				isSorted = 1;
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
-		for (k = 0; k < size; k++)
-			printfuuuu	}
+	}
 }
