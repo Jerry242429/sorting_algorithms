939431cad0270ba7fb39aa951aebebeda33939bb
Nardos <contactnardos@gmail.com>
2023-12-19 22:36:12 +0000

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list -  sort a list in ascending order
+ * @list: the random list
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+	size_t isSorted = 1, i = 0, j = 0, temp;
+
+	if (!list || !*list || !*list->next)
+		return;
+
+}
+
