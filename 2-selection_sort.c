47f0c211c9b5ce6e2f6c373bb372819dfc46e424
Jerry242429 <eyerusalem008@gmail.com>
2023-12-19 16:35:50 -0800

    sorting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,44 +1,34 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * selection_sort - Sort an array of integers in ascending order
- *                  using the selection sort algorithm.
- * @array: An array of integers.
+ * selection_sort -  sorts an array of integers in ascending order 
+ * using the Selection sort algorithm
+ * @array: array of integer.
  * @size: The size of the array.
+ * Return: void.
  *
- * Description: Prints the array after each swap.
  */
 void selection_sort(int *array, size_t size)
 {
-	int *min;
-	size_t i, j;
+	int a = 0;
+	size_t k = 0, j = 0, p = 0;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || size == 0)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (; i < size - 1; i++)
 	{
-		min = array + i;
-		for (j = i + 1; j < size; j++)
-			min = (array[j] < *min) ? (array + j) : min;
-
-		if ((array + i) != min)
+		p = k;
+		for (j = k + 1; j < size; j++)
 		{
-			swap_ints(array + i, min);
+			if (array[j] < array[pos])
+				p = j;
+		}
+		if (p != k)
+		{
+			aux = array[k];
+			array[k] = array[p];
+			array[p] = a;
 			print_array(array, size);
 		}
 	}
